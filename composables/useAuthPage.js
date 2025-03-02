3cd78a6741cf9e95714c4fce2a8b1a851b247c33
AMRWERY <amrmounir2@gmail.com>
2025-03-02 12:50:14 +0200

    .

--- composables/useAuthPage.js
+++
@@ -1,9 +0,0 @@
-export const useAuthPage = () => {
-  const route = useRoute();
-
-  const isAuthPage = computed(() =>
-    ["/sign-up", "/login"].includes(route.path)
-  );
-
-  return { isAuthPage };
-};
--- composables/useFormatPrice.js
+++
@@ -1,9 +0,0 @@
-export const useFormatPrice = () => {
-  const formatPrice = (price) => {
-    return parseFloat(price).toFixed(2);
-  };
-
-  return {
-    formatPrice,
-  };
-};
